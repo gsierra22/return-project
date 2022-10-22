6d02d6d92979d41b4387a495e30a756f28f76f67
gsierra22 <giancarlosierra2011@hotmail.com>
2022-10-22 13:45:36 -0500

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 import Body from './components/Body/Body';
+import axios from 'axios';
+import {useEffect} from 'react';
 function App() {
+
+  useEffect(()=>{
+    getName();
+},[]);
+
+  const getName =()=>{
+    console.log('in getName');
+  }
+
   return (
     <div className="App">
       <div>Willkommen!</div>
+      <Body test={3*9}/>
     </div>
   );
 }
