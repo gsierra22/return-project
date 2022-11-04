d79be2ddac527a21c62da4f7ac181ae570f005b7
gsierra22 <giancarlosierra2011@hotmail.com>
2022-11-03 23:04:06 -0500

    delete function works

--- server/routes/players.route.js
+++ server/routes/players.route.js
@@ -4,15 +4,14 @@ const pool = require('../modules/pool');
 
 router.delete('/:id', (req, res)=>{
     console.log('/todo delete', req.params);
-    res.send('delete');
-    // const queryString = `INSERT INTO "team1"("name","position","bio") VALUES ($1, $2, $3);`
-    // const values = [req.body.name, req.body.position, req.body.bio];
-    // pool.query(queryString, values).then((results)=>{
-    //     res.send(results.rows);
-    // }).catch((err)=>{
-    //     console.log(err);
-    //     res.sendStatus(500);
-    // })
+    const queryString = `DELETE FROM "team1" WHERE id=$1;`
+    const values = [req.params.id];
+    pool.query(queryString, values).then((results)=>{
+        res.send(200);
+    }).catch((err)=>{
+        console.log(err);
+        res.sendStatus(500);
+    })
 })
 
 router.get('/', (req, res)=>{
