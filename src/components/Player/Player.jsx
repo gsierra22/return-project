7915c1ccc7485f2524f3e638cb1b83ba7043dbe2
gsierra22 <giancarlosierra2011@hotmail.com>
2022-11-06 23:33:32 -0600

    update halfway

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -4,6 +4,16 @@ import axios from "axios";
 
 function Player(props){
     // const[name, setName]= useState(null);
+    const changeName = () =>{
+        axios.put(`/players/${props.player.id}`).then ((response) =>{
+            console.log('PUT', response);
+            props.getName();
+        }).catch((err)=>{
+            alert('put error');
+            console.log(err);
+        })
+    }
+    
     const deletePlayer = () =>{
         axios.delete(`/players/${props.player.id}`).then ((response) =>{
             console.log('delete', response);
@@ -17,6 +27,7 @@ function Player(props){
         <div>
             <h1>{props.player.name}</h1>
             <p>{props.player.position}</p>
+            <button onClick={changeName}>Change Name</button>
             <button onClick={deletePlayer}>Delete</button>
         </div>
     )
