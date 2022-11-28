0ccce79ec6abd540b08e003d4f7d5520cc3fde6b
gsierra22 <giancarlosierra2011@hotmail.com>
2022-11-27 23:06:44 -0600

    added functions to team2

--- src/components/Team2/Team2.jsx
+++ src/components/Team2/Team2.jsx
@@ -5,12 +5,30 @@ import {Button} from 'react-bootstrap';
 
 function Team2(props){
     // const[name, setName]= useState(null);
+    const changeName = () =>{
+        axios.put(`/players/${props.player.id}`).then ((response) =>{
+            props.getName();
+        }).catch((err)=>{
+            alert('put error');
+            console.log(err);
+        })
+    }
+    
+    const deletePlayer = () =>{
+        axios.delete(`/players/${props.player.id}`).then ((response) =>{
+            props.getName();
+        }).catch((err)=>{
+            alert('delete error');
+            console.log(err);
+        })
+    }
+
     return(
         <div>
             <h1>{props.player.name}</h1>
             <p>{props.player.position}</p>
-            <Button>Change Name</Button>
-            <Button variant="primary">Delete</Button>
+            <Button onClick={changeName}>Change Name</Button>
+            <Button variant="primary" onClick={deletePlayer}>Delete</Button>
         </div>
     )
 }
